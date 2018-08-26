e2422d6a5bb30915191e20f284c66276f00e8670
trolly <garipovtursun@gmail.com>
2018-08-26 11:16:44 +0500

    Init module

---
+++ src/providers/ModuleProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tvolly\backend\providers;
+
+use tvolly\core\base\BaseProvider;
+
+class ModuleProvider extends BaseProvider
+{
+    public static function getName()
+    {
+        return 'backend';
+    }
+}
